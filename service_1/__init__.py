da6d79dd879e4d340a06678d6d1063580dee26e9
Ubuntu <ecalim@serverless.lln0njcjfpvexkjq3mlfd2sole.zx.internal.cloudapp.net>
2020-12-11 15:03:06 +0000

    service 1 added and application deployed

--- service_1/__init__.py
+++ service_1/__init__.py
@@ -1,24 +1,15 @@
 import logging
-
+import requests 
 import azure.functions as func
-
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    letter = requests.get(https://efekancalimserverless.azurewebsites.net/api/service_3)
+    number = requests.get(https://efekancalimserverless.azurewebsites.net/api/service_2)
+    username = ""
+    for i in range(5):
+        username += letter[i]
+        username += number[i]
+    return username
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+   
